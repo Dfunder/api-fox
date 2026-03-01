cdf8933e5afb98d24e97717798e1de52281d531b
sudiptapaul <psudipta858@gmail.com>
2026-03-01 18:41:02 +0530

    project structure added

---
+++ src/config/db.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(` MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(" MongoDB connection failed:", error.message);
+
+    process.exit(1);
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.warn(" MongoDB disconnected. Attempting to reconnect...");
+});
+
+mongoose.connection.on("reconnected", () => {
+  console.log("MongoDB reconnected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB error:", err);
+});
+
+module.exports = connectDB;
